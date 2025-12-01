cb81c72a5ec50f490ba1df9566809565d0bc87ef
Óliver Ros <105983648+oliverdev75@users.noreply.github.com>
2025-12-01 16:41:01 +0100

    Input changes

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 function Input ({ name, type = "string", placeholder = null }: Props) {
-    return <input type={type} name={name} className="px-10" />
+    return <input type={type} name={name} className="px-5 py-4 rounded border" />
 }
 
 export default Input
