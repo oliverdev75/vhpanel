d8626e53cde06b2847e4e4d599350a47ff7bd343
Óliver Ros <105983648+twister91@users.noreply.github.com>
2025-12-01 16:26:45 +0100

    Essential components, login (in progress)

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,7 +7,14 @@ interface Props {
 }
 
 function Input ({ name, type = "string", placeholder = null }: Props) {
-    return <input type={type} name={name} className="px-10" />
+    return (
+        <input
+            type={type}
+            name={name}
+            {...(placeholder && { placeholder: placeholder })}
+            className="px-3 py-1 rounded-lg border border-gray-500"
+        />
+    )
 }
 
 export default Input
